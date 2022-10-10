2421df6ec5cf828c7f632b5588fd2fa8829e5f2a
Themis <vasiliouths@gmail.com>
2022-10-11 02:06:05 +0300

    base backend config and react install

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const keys = require("./config/keys");
+const passport = require("passport");
+require("./services/passport");
+require("./models/User");
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send({ hi: "there" });
-});
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey],
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+equire("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
